43d2446e157b7e83b288d3a6bd4ea737e8ba64ed
Indra <Indra@192.168.0.125>
2015-12-28 16:49:00 +0000

    Kata1

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -1,28 +1,24 @@
 package kata2;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Histogram <T> {
 
-    private final T[] vector;
-
-    public Histogram(T[] vector) {
-        this.vector = vector;
+    private final Map<T,Integer>  histogram = new HashMap<>();
+    
+    public Integer get(Object key){
+        return histogram.get(key);
     }
     
-    public Object[] getVector() {
-        return vector;
+    public Set<T> keySet(){
+        return histogram.keySet();
     }
     
-    public HashMap<T,Integer> getHistogram(){
-        HashMap<T,Integer> histogram = new HashMap<T, Integer>();
-        for (int i = 0; i < vector.length; i++) {
-            if(!histogram.containsKey(vector[i])){
-                histogram.put(vector[i],1);
-            } else {
-                histogram.put(vector[i], histogram.get(vector[i])+1);
-            }
-        }
-        return histogram;
+    public Integer increment(T key){
+        return histogram.put(key, histogram.containsKey(key) ? histogram.get(key)+1 : 1);
     }
 }
+
+
